a83c68f49fb09584610a35551c64a4769c8e4ae0
gwalchmay <l.luck@laposte.net>
2020-04-14 11:44:16 +0200

    finished React quest

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
 import React from 'react';
+import Navbar from './components/Navbar';
+import QuoteCard from './components/QuoteCard';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      character: 'Nelson Muntz',
+      image: 'https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FNelsonMuntz.png?1497567511185',
+      quote: 'Shoplifting is a victimless crime, like punching someone in the dark.',
+    };
+    this.getQuote = this.getQuote.bind(this);
+  }
+
+  getQuote() {
+    // Send the request
+    axios.get('https://simpsons-quotes-api.herokuapp.com/quotes')
+      // Extract the DATA from the received response
+      .then(response => response.data)
+      // Use this data to update the state
+      .then(data => {
+        this.setState({
+          quote: data[0].quote,
+          character: data[0].character,
+          image: data[0].image,
+        });
+      });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <QuoteCard quote={this.state.quote} image={this.state.image} character={this.state.character} />
+        <button type="button" onClick={this.getQuote}>Get another quote!</button>
+      </div>
+    );
+  }
 }
+
 
 export default App;
